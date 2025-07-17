d75c20ab0261e3a066a530b0af872a03daa05b36
simonanang <simonanang@gmail.com>
2025-07-17 17:13:00 +0000

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752772379683

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_context_82a4e9a3._.js"
+],
+    source: "dynamic"
+});
