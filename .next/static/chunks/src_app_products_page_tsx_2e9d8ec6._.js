0477c940db396bc802e1ada32bee6ae675ef2055
simonanang <simonanang@gmail.com>
2025-07-17 17:12:03 +0000

    Add .next/static/chunks/src_app_products_page_tsx_2e9d8ec6._.js 1752772322787

---
+++ .next/static/chunks/src_app_products_page_tsx_2e9d8ec6._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_d765e8b9._.js"
+],
+    source: "dynamic"
+});
